42819a91231991d349af52b134e08c429699fa61
jazzari <passionjazz@gmail.com>
2020-04-29 18:23:05 +0200

    Adding comments to the articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcame/index'
 
-  resources :articles
+  resources :articles do
+  	resources :comments
+  end
 
   root 'welcame#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
